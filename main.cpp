b478bbb4fcacd32dfc31a7cfc1049a611c8e3833
Ramón Alberto Gómez Urquídez <gomez.ramon2004@gmail.com>
2023-10-15 22:15:55 -0700

    added three functionalities testcases

--- main.cpp
+++ main.cpp
@@ -1,39 +1,39 @@
 #include "./bst/bst.hpp"
+#include "./testcase/testcase.hpp"
 #include <iostream>
 #include <string>
 
-// In this version of the priority queue, I'll use nodes as a container for the dat
-
 int main() {
     try {
+        // Binary Search Tree Basic Structure - For the testcases
+
         BST bst;
-        bst.insertNode(21);
-        bst.insertNode(13);
-        bst.insertNode(33);
-        bst.insertNode(10);
-        bst.insertNode(18);
-        bst.insertNode(25);
-        bst.insertNode(40);
-        //bst.insertNode(1);
+        addNodes(bst);
 
-        std::cout << "Top: " << bst.root->data << std::endl;
-        std::cout << bst.root->left->data << std::endl;
-        std::cout << bst.root->right->data << std::endl;
-        std::cout << bst.root->left->left->data << std::endl;
-        std::cout << bst.root->left->right->data << std::endl;
-        std::cout << bst.root->right->left->data << std::endl;
-        std::cout << bst.root->right->right->data << std::endl;
+        // Visit Testcases
 
+        bst.visit(1);   // First Case
+        bst.visit(2);   // Second Case
+        bst.visit(3);   // Third Case
+        bst.visit(4);   // Four Case
+
+        // Height Testcases
+
+        std::cout << bst.height() << std::endl;     // First Case
+        bst.insertNode(1);                          // Second Case
         std::cout << bst.height() << std::endl;
-        //std::cout << bst.whatlevelamI(1) << std::endl;
-        std::cout << bst.whatlevelamI(18) << std::endl;
-        std::cout << bst.whatlevelamI(40) << std::endl;
+        bst.deleteNode(1), bst.deleteNode(10), bst.deleteNode(18), bst.deleteNode(25), bst.deleteNode(40);      // Third Case
+        std::cout << bst.height() << std::endl;
+        bst.deleteNode(13), bst.deleteNode(33);     // Fourth Case
+        std::cout << bst.height() << std::endl;
 
-        //bst.ancestors(1);
-        bst.visit(1);
-        bst.visit(2);
-        bst.visit(3);
-        bst.visit(4);
+        // Ancestors Testcases
+        bst.deleteNode(21);
+        addNodes(bst);
+        bst.ancestors(10);      // First Case
+        bst.ancestors(18);      // Second Case
+        bst.ancestors(25);      // Third Case
+        bst.ancestors(40);      // Fourth Case
 
     } catch (const std::runtime_error &e) {
         std::cerr << e.what() << std::endl;
